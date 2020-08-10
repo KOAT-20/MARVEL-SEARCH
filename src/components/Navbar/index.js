e68d556e72b4e7686ba06ea10da6144ef4dc2070
Kevin Alvarez <45703363+KOAT-20@users.noreply.github.com>
2020-08-10 18:11:08 -0400

    corrección en Link

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,7 +7,7 @@ export default function Navbar (props) {
   return (
     <nav className='navbar navbar-expand-lg navbar-light bg-light'>
       <div className='container-fluid'>
-        <Link>
+        <Link to='/'>
           <img src={logo} className='navbar-brand' />
         </Link>
         <button
